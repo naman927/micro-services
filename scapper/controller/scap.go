c4dc92d7302461dc9cf412f249aeca44e4f18c33
naman927 <42885108+naman927@users.noreply.github.com>
2022-09-15 12:25:21 +0530

    fix: set the user-agent to get desired output

--- scapper/controller/scap.go
+++ scapper/controller/scap.go
@@ -129,6 +129,7 @@ func ScapWeb(c *gin.Context) {
 	})
 
 	scapper.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36")
 		fmt.Println("Visiting", r.URL)
 	})
 
